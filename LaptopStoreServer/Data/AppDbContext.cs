b2002582a68d962af3499f91a6d7e379f1bf433b
Trhaokiet1303 <Trhaokiet.1303@gmail.com>
2024-11-02 17:58:43 +0700

    first commit

---
+++ LaptopStoreServer/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using LaptopStoreSharedLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LaptopStoreServer.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; } = default!;
+    }
+}
---
+++ LaptopStoreSharedLibrary/Models/Product.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace LaptopStoreSharedLibrary.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+        
+        [Required]
+        public string? Description { get; set; }
+        
+        [Required, Range(0.1, 99999.99)]
+        public string? Manufacturer { get; set; }
+        
+        [Required]
+        public string? ManufacturerName { get; set; }
+        
+        [Required]
+        public decimal Price { get; set; }
+        
+        [Required, DisplayName("Product Image")]
+        public string? Base64img { get; set; }
+
+        public int Quantity { get; set; }
+
+        public bool Featured { get; set; }
+
+        public DateTime dateUpload { get; set; } = DateTime.Now;
+    }
+}
