742ece38ffa617cf2c3b1b9bc12eb54f701defdc
Trhaokiet1303 <Trhaokiet.1303@gmail.com>
2024-11-28 01:04:51 +0700

    edit account

--- Client/Shared/Shop/ShopLayout.razor.cs
+++ Client/Shared/Shop/ShopLayout.razor.cs
@@ -1,18 +1,114 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using LaptopStore.Client.Extensions;
+using LaptopStore.Client.Infrastructure.Managers.Identity.Roles;
+using LaptopStore.Shared.Constants.Application;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.SignalR.Client;
+using System.Threading.Tasks;
+using MudBlazor;
+using System.Linq;
+using System.Net.Http.Headers;
+using System;
+
 
 namespace LaptopStore.Client.Shared.Shop
 {
     public partial class ShopLayout : LayoutComponentBase
     {
         [Inject] private NavigationManager NavigationManager { get; set; }
+        [Inject] private IRoleManager RoleManager { get; set; }
+
+        private string CurrentUserId { get; set; }
+        private string ImageDataUrl { get; set; }
+        private string FirstName { get; set; }
+        private string SecondName { get; set; }
+        private string Email { get; set; }
+        private char FirstLetterOfName { get; set; }
 
         private string PageTitle { get; set; }
-
-        protected override void OnInitialized()
+        private HubConnection hubConnection;
+        public bool IsConnected => hubConnection.State == HubConnectionState.Connected;
+        protected override async Task OnInitializedAsync()
         {
             // Lấy thông tin cơ bản về trang hiện tại (ví dụ: URL để tùy chỉnh tiêu đề)
             var currentUri = NavigationManager.Uri;
             PageTitle = currentUri.Contains("home") ? "Home Page" : "Shop Page";
+            _interceptor.RegisterEvent();
+            hubConnection = hubConnection.TryInitialize(_navigationManager);
+            await hubConnection.StartAsync();
+            await LoadDataAsync();
+            hubConnection.On(ApplicationConstants.SignalR.ReceiveRegenerateTokens, async () =>
+            {
+                try
+                {
+                    var token = await _authenticationManager.TryForceRefreshToken();
+                    if (!string.IsNullOrEmpty(token))
+                    {
+                        _snackBar.Add(localizer["Refreshed Token."], Severity.Success);
+                        _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    _snackBar.Add(localizer["You are Logged Out."], Severity.Error);
+                    await _authenticationManager.Logout();
+                    _navigationManager.NavigateTo("/");
+                }
+            });
+
+            hubConnection.On<string, string>(ApplicationConstants.SignalR.LogoutUsersByRole, async (userId, roleId) =>
+            {
+                if (CurrentUserId != userId)
+                {
+                    var rolesResponse = await RoleManager.GetRolesAsync();
+                    if (rolesResponse.Succeeded)
+                    {
+                        var role = rolesResponse.Data.FirstOrDefault(x => x.Id == roleId);
+                        if (role != null)
+                        {
+                            var currentUserRolesResponse = await _userManager.GetRolesAsync(CurrentUserId);
+                            if (currentUserRolesResponse.Succeeded && currentUserRolesResponse.Data.UserRoles.Any(x => x.RoleName == role.Name))
+                            {
+                                _snackBar.Add(localizer["You are logged out because the Permissions of one of your Roles have been updated."], Severity.Error);
+                                await hubConnection.SendAsync(ApplicationConstants.SignalR.OnDisconnect, CurrentUserId);
+                                await _authenticationManager.Logout();
+                                _navigationManager.NavigateTo("/login");
+                            }
+                        }
+                    }
+                }
+            });
+        }
+        private async Task LoadDataAsync()
+        {
+            var state = await _stateProvider.GetAuthenticationStateAsync();
+            var user = state.User;
+            if (user == null) return;
+            if (user.Identity?.IsAuthenticated == true)
+            {
+                CurrentUserId = user.GetUserId();
+                FirstName = user.GetFirstName();
+                if (FirstName.Length > 0)
+                {
+                    FirstLetterOfName = FirstName[0];
+                }
+                SecondName = user.GetLastName();
+                Email = user.GetEmail();
+                var imageResponse = await _accountManager.GetProfilePictureAsync(CurrentUserId);
+                if (imageResponse.Succeeded)
+                {
+                    ImageDataUrl = imageResponse.Data;
+                }
+
+                var currentUserResult = await _userManager.GetAsync(CurrentUserId);
+                if (!currentUserResult.Succeeded || currentUserResult.Data == null)
+                {
+                    _snackBar.Add(localizer["You are logged out because the user with your Token has been deleted."], Severity.Error);
+                    await _authenticationManager.Logout();
+                }
+
+                await hubConnection.SendAsync(ApplicationConstants.SignalR.OnConnect, CurrentUserId);
+            }
         }
     }
 }
